9273d2b948eb122deae40810b18430b1c5eba15d
VarelajoseAlberto <varelaj298@gmail.com>
2026-04-17 19:21:19 -0300

    completado parte 2 punto 2 funcion buscar por palabra clave

--- ArregloDeNombre.c
+++ ArregloDeNombre.c
@@ -4,6 +4,8 @@
 
 // declaracion de funcion para mostrar nombres
 void MostrarPersonas(char *nombres[]);
+// funcion para buscar nombre por palabra clave
+int BuscarNombrePorPalabra(char *nombre[], char palabra[]);
 
 int main(int argc, char const *argv[])
 {
@@ -12,6 +14,9 @@ int main(int argc, char const *argv[])
     char buff[50]; // buffer aux para guardar temporalmente los nombres
 
     int cantLetraNomb; // variable que contendra la cantidad de letra del nombre
+
+    char palabra[50]; // idem al buff
+    int posicion; // variable donde contendra la posicion encontrada de la funcion buscarpornombre
 
     // bucle para ingresar los nombres de los alumnos
     for (int i = 0; i < 5; i++)
@@ -23,11 +28,27 @@ int main(int argc, char const *argv[])
         cantLetraNomb = strlen(buff);                                    // calcula la longitud del nombre ingresado
         nombreDeAlumnos[i] = malloc(sizeof(char) * (cantLetraNomb + 1)); // reservo memoria dinamica y el +1 es para cararter nulo '\0' que indica el fin de la cadena
         strcpy(nombreDeAlumnos[i], buff);                                // copia el contenido del buffer a la memoria reservada
-        //nota si cuando me pida un nombre y solo precione ENTER dicho
-        //ENTER se guardara como un nombre mas
+        // nota si cuando me pida un nombre y solo precione ENTER dicho
+        // ENTER se guardara como un nombre mas
     }
 
     MostrarPersonas(nombreDeAlumnos);
+
+    printf("Ingrese palabra clave: ");
+    fgets(palabra, sizeof(palabra), stdin);
+    palabra[strcspn(palabra, "\n")] = '\0';
+
+    posicion=BuscarNombrePorPalabra(nombreDeAlumnos,palabra);
+
+    if (posicion!=1)
+    {
+        printf("nombre encontrado: %s\n",nombreDeAlumnos[posicion]);
+    }
+    else
+    {
+        printf("no se encontro coincidencia.\n");
+    }
+    
 
     // for para liberar la memoria dinamica reservada
     for (int i = 0; i < 5; i++)
@@ -43,4 +64,19 @@ void MostrarPersonas(char *nombres[])
     {
         printf("Nombre del alumno N°%d: %s\n", i + 1, nombres[i]);
     }
+}
+
+//el metodo funciona hasta que encuentra la primera coinsidencia
+// si hay mas de un nombre similar solo tomara el primero que encuentre
+int BuscarNombrePorPalabra(char *nombre[], char palabra[])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (strstr(nombre[i], palabra) != NULL)
+        {
+            return i; // devuelde la posicion encontrada
+        }
+    }
+
+    return -1; // no encontro coincidencia
 }
